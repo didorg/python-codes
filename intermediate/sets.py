059dfd0822170816365681070da788bc467e484c
Didier <didorg@gmail.com>
2020-12-18 20:45:29 -0500

    Update sets.py

--- intermediate/sets.py
+++ intermediate/sets.py
@@ -1,2 +1,33 @@
 # Sets: Unordered, mutable, no duplicates
 
+mySet = {1, 2, 2, 3, 4, 5, 6}
+print(mySet)  # R/ {1, 2}
+
+mySet1 = set("Hello")
+print(mySet1)  # R/ {'H', 'l', 'o', 'e'} / order is arbitrary and only one l.
+
+# Empty Set
+mySet2 = set()
+# mySet2 = {} # This is a Dictionary.
+
+mySet2.add("uno")
+mySet2.add("dos")
+
+# discard() does the same as remove(), but If the element is not a member, do nothing.
+mySet2.discard("dos")
+print(mySet2)
+print(type(mySet2))
+
+mySet3 = {5, 6, 7, 8, 9}
+
+# union(): Return the union of sets as a new set
+unionSet = mySet.union(mySet3)
+print(unionSet)
+
+# intersection(): Return the intersection of two sets as a new set.
+iSet = mySet.intersection(mySet3)
+print(iSet)
+
+# difference(): Return the difference of two or more sets as a new set.
+diffSet = mySet.difference(mySet3)
+print(diffSet)
