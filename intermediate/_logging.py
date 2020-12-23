fb1b60fcb1a7f321e5de72f35a40995dba5897c3
Didier <didorg@gmail.com>
2020-12-23 13:41:20 -0500

    Update _logging.py

--- intermediate/_logging.py
+++ intermediate/_logging.py
@@ -1,7 +1,10 @@
 import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S')
+
+
 logging.debug('This is a debug message')
 logging.info('This is an info message')
 logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
-
