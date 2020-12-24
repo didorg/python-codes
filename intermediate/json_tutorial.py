d6cb5bf9e02322f27c72a829001ac5c5bfc8676a
Didier <didorg@gmail.com>
2020-12-24 15:29:17 -0500

    update json_tutorial

--- intermediate/json_tutorial.py
+++ intermediate/json_tutorial.py
@@ -8,4 +8,13 @@ print(carJson)
 
 # dump(): Serialize obj as a JSON formatted stream to fp
 with open('../FILES/car.json', 'w') as file:
-    json.dump(car, file)
+    json.dump(car, file, indent=4)
+
+# loads(): Deserialize s (a str, bytes or bytearray instance containing a JSON document) to a Python object.
+car = json.loads(carJson)
+print(car)
+
+# load(): Deserialize fp (a .read()-supporting file-like object containing a JSON document) to a Python object.
+with open('../FILES/car.json', 'r') as file:
+    car = json.load(file)
+    print(car)
