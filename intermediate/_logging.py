dadb037e060295b5b53b7117a8083b8c884cc8d2
Didier <didorg@gmail.com>
2020-12-23 19:00:57 -0500

    Update _logging.py

--- intermediate/_logging.py
+++ intermediate/_logging.py
@@ -1,10 +1,34 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S')
-
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 
 logging.debug('This is a debug message')
 logging.info('This is an info message')
 logging.warning('This is a warning message')
 logging.error('This is an error message')
 logging.critical('This is a critical message')
+
+
+# The following example configures a very simple logger, a console handler, and a simple formatter using Python code:
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# 'application' code
+logger.info('------------------- A very simple logger --------------------')
+logger.debug('debug message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
