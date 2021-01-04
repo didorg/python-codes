a0a86cd657f2d4cab02976cc3f23c5e0aad2fb52
Didier <didorg@gmail.com>
2021-01-04 18:00:50 -0500

    Update function_arguments.py

--- intermediate/function_arguments.py
+++ intermediate/function_arguments.py
@@ -1,4 +1,4 @@
-def foo(a, b, *args, **kwargs):
+def foo(a, b, *args, **kwargs):  # *args is a tuple. **kwargs is a dictionary
     print(a, b)
     for arg in args:
         print(arg)
@@ -7,3 +7,14 @@ def foo(a, b, *args, **kwargs):
 
 
 foo(1, 2, 3, 4, 5, six=6, seven=7)
+
+
+def boo(a, b, c):
+    print(a, b, c)
+
+
+my_list = (9, 6, 3)
+boo(*my_list)
+
+my_dictionary = {'a': 2, 'b': 4, 'c': 6}
+boo(**my_dictionary)
