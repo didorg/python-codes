3a85e8d218bf6babc8e8346054d3c027baa7e64d
Didier <didorg@gmail.com>
2021-03-15 00:40:50 -0400

    Update class_fundations.py

--- basics/class_fundations.py
+++ basics/class_fundations.py
@@ -3,9 +3,13 @@ class Person:
         self.name = name
         self.age = age
 
+    def speak(self):
+        print(f'{self.name} is speaking')
+
     def walk(self):
-        print(self.name + ' is walking')
+        print(f'{self.name} is walking')
 
 
 john = Person('John', 25)
+john.speak()
 john.walk()
