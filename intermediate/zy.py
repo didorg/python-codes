6180cc38859169a03ae9ab8af955f531e85ef1e7
Didier <didorg@gmail.com>
2021-04-20 19:11:35 -0400

    Update zy.py

--- intermediate/zy.py
+++ intermediate/zy.py
@@ -1,18 +1,21 @@
+import sqlite3
+
+
 def split_value(day_rang, idx):
     arr = [x for x in day_rang.split(" - ")]
     return arr[idx]
 
 
-
-day_rang = '133.34 - 135.47'
-
-low = split_value(day_rang, 0)
-high = split_value(day_rang, 1)
-vol = '68,710,681'
-result = vol.replace(',', '')
-fl = float(result)
-print(fl)
-print(type(fl))
+def replace_vol(str_vol):
+    if 'M' in str_vol:
+        return float(str_vol.replace('M', '0000').replace('.', ''))
+    if 'K' in str_vol:
+        return float(vol_k.replace('K', '0').replace('.', ''))
 
 
+vol_m = '65.72M'  # 65720000
+vol_k = '675.08K'  # 675080
 
+# print(replace_vol(vol_k))
+
+print(sqlite3.sqlite_version)
